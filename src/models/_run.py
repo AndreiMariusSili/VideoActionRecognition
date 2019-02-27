e1400425a2a243c61e155e47b35c58215590d84c
andrei sili <andrei.sili1994@gmail.com>
2019-02-27 16:37:45 +0100

    Added more logging to a run.

--- src/models/_run.py
+++ src/models/_run.py
@@ -1,5 +1,6 @@
+import json
 import os
-from typing import Any, Optional, Union, Dict
+from typing import Any, Optional, Union, Dict, Type, Tuple
 from ignite import engine, handlers
 from torch import nn, cuda, optim
 import dataclasses as dc
@@ -29,28 +30,31 @@ class Run(object):
     resume: bool
     name: str
 
+    logger: logging.Logger
+
     def __init__(self, opts: models.RunOptions):
-        logging.info(f'Initializing run {opts.name}.')
+        self.__init_record(opts)
+        self.logger = self.__init_logging(opts.name)
+        self.logger.info(f'Initializing run {opts.name}.')
+
         self.name = opts.name
         self.resume = opts.resume
         self.resume_from = opts.resume_from
         self.epochs = opts.trainer_opts.epochs
 
         self.model = self.__init_model(opts.model, opts.model_opts)
-        logging.info(self.model)
-        self.optimizer = opts.trainer_opts.optimizer(self.model.parameters(),
-                                                     **dc.asdict(opts.trainer_opts.optimizer_opts))
-        logging.info(self.optimizer)
+        self.logger.info(self.model)
+        self.optimizer = self.__init_optimizer(opts.trainer_opts.optimizer, opts.trainer_opts.optimizer_opts)
+        self.logger.info(self.optimizer)
         self.criterion = opts.trainer_opts.criterion(**dc.asdict(opts.trainer_opts.criterion_opts))
-        logging.info(self.criterion)
+        self.logger.info(self.criterion)
         self.data_bunch = opts.data_bunch(opts.data_bunch_opts, opts.data_set_opts, opts.data_loader_opts)
-        logging.info(self.data_bunch)
+        self.logger.info(self.data_bunch)
 
         self.metrics = opts.evaluator_opts.metrics
         self.device = th.device('cuda' if th.cuda.is_available() else 'cpu')
 
-        self.trainer = engine.create_supervised_trainer(self.model, self.optimizer, self.criterion, self.device, True)
-        self.evaluator = engine.create_supervised_evaluator(self.model, self.metrics, self.device, True)
+        self.trainer, self.evaluator = self.__init_trainer_evaluator()
 
         self.log_interval = opts.log_interval
         self.iterations = math.ceil(len(self.data_bunch.train_set) / self.data_bunch.dl_opts.batch_size)
@@ -58,12 +62,44 @@ class Run(object):
         self.__init_handlers()
         self.__init_stats_recorder()
 
+    def __init_record(self, opts: models.RunOptions):
+        os.makedirs(ct.RUN_DIR / opts.name, exist_ok=True)
+        with open(ct.RUN_DIR / opts.name / 'run.json', 'w') as file:
+            json.dump(dc.asdict(opts), file, indent=True, sort_keys=True, default=str)
+
+    def __init_logging(self, run: str):
+        logger = logging.getLogger()
+        formatter = logging.Formatter('[%(asctime)-s][%(process)d][%(levelname)s]\t%(message)s')
+        file_handler = logging.FileHandler(ct.RUN_DIR / run / 'run.log', encoding='utf-8')
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+        return logger
+
     def __init_model(self, model: nn.Module, opts: Any) -> nn.Module:
         model = model(**dc.asdict(opts))
         if self.resume:
-            logging.info(f'Loading model from {self.resume_from}.')
-            model.load_state_dict(th.load(self.resume_from))
+            self.logger.info(f'Loading model from {self.resume_from}.')
+            model.load_state_dict(th.load(self.resume_from)['model'])
+
         return model
+
+    def __init_optimizer(self, optimizer: Type[optim.Optimizer], optimizer_opts: Any) -> optim.Optimizer:
+        optimizer = optimizer(self.model.parameters(), **dc.asdict(optimizer_opts))
+        if self.resume:
+            self.logger.info(f'Loading optimizer from {self.resume_from}.')
+            optimizer.load_state_dict(th.load(self.resume_from)['optimizer'])
+
+        return optimizer
+
+    def __init_trainer_evaluator(self) -> Tuple[engine.Engine, engine.Engine]:
+        trainer = engine.create_supervised_trainer(self.model, self.optimizer, self.criterion, self.device, True)
+        if self.resume:
+            trainer.state = th.load(self.resume_from)['trainer_state']
+        evaluator = engine.create_supervised_evaluator(self.model, self.metrics, self.device, True)
+        if self.resume:
+            evaluator.state = th.load(self.resume_from)['evaluator_state']
+
+        return trainer, evaluator
 
     def __init_handlers(self) -> None:
         self.__init_global_timer_handler()
@@ -108,10 +144,10 @@ class Run(object):
 
     def _on_iteration_completed(self, _engine: engine.Engine) -> None:
         iteration = (_engine.state.iteration - 1) % len(self.data_bunch.train_set) + 1
-        if iteration % self.log_interval >= 0:
-            logging.info(f'[Batch: {_engine.state.iteration:04d}/{self.iterations:4d}]'
-                         f'[Iteration Time: {self.iter_timer.value():6.2f}s]'
-                         f'[Batch Loss: {_engine.state.output:8.4f}]')
+        if iteration % self.log_interval == 0:
+            self.logger.info(f'[Batch: {_engine.state.iteration:04d}/{self.iterations:4d}]'
+                             f'[Iteration Time: {self.iter_timer.value():6.2f}s]'
+                             f'[Batch Loss: {_engine.state.output:8.4f}]')
 
     def _on_epoch_completed(self, _engine: engine.Engine) -> None:
         metrics = {}
