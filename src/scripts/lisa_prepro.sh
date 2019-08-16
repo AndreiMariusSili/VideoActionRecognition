8838ed531ce6b984d849e96140db8c5113311acd
andrei sili <andrei.sili1994@gmail.com>
2019-08-16 04:08:22 +0300

    Updated bash scripts.

---
+++ src/scripts/lisa_prepro.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH -p normal
+#SBATCH -n 16
+#SBATCH -t 12:00:00
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --mail-user=andrei.sili1994@gmail.com
+
+# Source profile
+source "${HOME}/.bash_profile"
+
+# Activate environment
+conda activate mt
+
+# Set environment variables.
+echo "Setting environment variables..."
+export MT_ROOT="${HOME}/master-thesis"
+
+# Run program
+echo "Running program..."
+python "${MT_ROOT}/src/main.py" "prepro_set" "-o" "set:smth,jpeg:1"
